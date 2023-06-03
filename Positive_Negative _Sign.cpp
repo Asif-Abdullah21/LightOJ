c26265bf246f8bda703a17522817a60b82ceaa9e
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-06-03 23:04:15 +0600

    LOJ-1294

---
+++ Positive_Negative _Sign.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ss " "
+#define sort(a) sort(a, a + n)
+#define rev(a) reverse(a, a + n)
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sz(v) v.size()
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    FIO;
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        cout << "Case " << i << ": ";
+        ll n,m;
+        cin >> n >> m;
+        cout << (n/2)*m<< endl;
+    }
+
+    return 0;
+}
